b536b8b6c91ed83f81f9156f1bd6899dfbc0b995
larn <drrg@naver.com>
2022-02-18 14:44:22 +0900

    2022.02.18 영양제 시간 알림 / 기온별 추천 옷차림 추가

--- src/main/java/com/larn/alarm/scheduler/MessageScheduler.java
+++ src/main/java/com/larn/alarm/scheduler/MessageScheduler.java
@@ -1,8 +1,5 @@
 package com.larn.alarm.scheduler;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -27,22 +24,32 @@ public class MessageScheduler {
 	@Autowired
 	WeaterInfoService weaterInfoService;
 
+	//@Scheduled(cron="0 00 07 * * ?")
 	@Scheduled(fixedRate = 5000)
     public void morningWeatherInfoScheduler() { // 아침 날씨 알람 스케쥴러
 		String linkUrl = "https://weather.naver.com/today";
 		String authToken = AuthService.getAuthToken();
 		logger.info("==============WeatherSceduler start=============="); // 추후 AOP로 Service Start 로그 관리
 		if(!StringUtils.isEmpty(authToken)) {
-			WeatherInfoDto weatherInfoDto = weaterInfoService.getWeaterInfo();
+			WeatherInfoDto weatherInfoDto = weaterInfoService.getWeatherInfo();
 			DefaultMessageDto msgDto = new DefaultMessageDto();
+
+			int temp = Integer.parseInt(weatherInfoDto.getTemp());
+			String yyyy = weatherInfoDto.getDay().substring(0, 4);
+			String mm = weatherInfoDto.getDay().substring(4, 6);
+			String dd = weatherInfoDto.getDay().substring(6, 8);
+			String time = weatherInfoDto.getTime();
+			time = time.substring(0,2) + ":" + time.substring(2,4);
+			String weatherStatus = weatherInfoDto.getWeatherStatus();
+			String recommandWear = weaterInfoService.getWearRecommandForWeather(temp);
+
+
 			StringBuilder sb = new StringBuilder();
-			Map<String,String> linkMap = new HashMap<>();
-
-
-			sb.append(weatherInfoDto.getDay() + "일 " + System.lineSeparator());
-			sb.append("발표시간 : " + weatherInfoDto.getTime() + " 기준  " + System.lineSeparator());
-			sb.append("현재온도 : " + weatherInfoDto.getTemp() + System.lineSeparator());
-			sb.append("현재기상 : " + weatherInfoDto.getWeatherStatus() + " 입니다. ");
+			sb.append(yyyy + "년 " + mm + "월 " + dd + "일 " + System.lineSeparator());
+			sb.append("발표시간 : " + time + "시 기준  " + System.lineSeparator());
+			sb.append("현재온도 : " + temp + System.lineSeparator());
+			sb.append("현재기상 : " + weatherStatus + " 입니다. " + System.lineSeparator());
+			sb.append("온도에 맞는 오늘의 추천의상은 : " + System.lineSeparator() + recommandWear + " 입니다. " + System.lineSeparator());
 
 			String msg = sb.toString();
 			msgDto.setObjType("text");
@@ -56,5 +63,25 @@ public class MessageScheduler {
 			throw new ServiceException("토큰정보가 없습니다!");
 		}
 
+	}
+
+	@Scheduled(cron="0 00 14 * * ?")
+	public void nutrientsAlarmScheduler() { // 영양제 알림 스케쥴러
+		String linkUrl = "";
+		String authToken = AuthService.getAuthToken();
+		logger.info("==============nutrientsAlarmSceduler start=============="); // 추후 AOP로 Service Start 로그 관리
+		if(!StringUtils.isEmpty(authToken)) {
+			DefaultMessageDto msgDto = new DefaultMessageDto();
+
+			msgDto.setObjType("text");
+			msgDto.setText("영양제를 먹을 시간입니다.");
+			msgDto.setBtnTitle("");
+			msgDto.setWebUrl(linkUrl);
+			msgDto.setMobileUrl(linkUrl);
+
+			msgService.sendMessage(authToken, msgDto);
+		}else {
+			throw new ServiceException("토큰정보가 없습니다!");
+		}
 	}
 }
--- src/main/java/com/larn/alarm/weather/service/WeaterInfoService.java
+++ src/main/java/com/larn/alarm/weather/service/WeaterInfoService.java
@@ -1,9 +1,7 @@
 package com.larn.alarm.weather.service;
 
 import java.time.LocalDate;
-import java.util.HashMap;
 import java.util.Locale;
-import java.util.Map;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
@@ -16,70 +14,105 @@ import org.springframework.web.util.UriComponentsBuilder;
 
 import com.larn.alarm.base.service.HttpCallService;
 import com.larn.alarm.exception.ServiceException;
+import com.larn.alarm.weather.dto.WearRecommandDto;
 import com.larn.alarm.weather.dto.WeatherInfoDto;
 
 @Service
 public class WeaterInfoService extends HttpCallService {
 	private static final String WHEATER_INFO_API_URL = "http://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getVilageFcst?";
 
-	@Autowired MessageSource msgSource;
+	@Autowired
+	MessageSource msgSource;
 
-	public WeatherInfoDto getWeaterInfo() {
+	public WeatherInfoDto getWeatherInfo() {
 		LocalDate now = LocalDate.now();
 		String todayDate = now.toString().replaceAll("-", "");
-		String apiKey= msgSource.getMessage("weather.api.key", null, Locale.getDefault());
+		String apiKey = msgSource.getMessage("weather.api.key", null, Locale.getDefault());
 		String rows = "10";
 		String pageNo = "1";
 		String baseTime = "0500";
 		String dataType = "json";
 
 		UriComponentsBuilder builder = UriComponentsBuilder.fromHttpUrl(WHEATER_INFO_API_URL)
-		        .queryParam("serviceKey", apiKey)
-		        .queryParam("numOfRows", rows)
-		        .queryParam("pageNo", pageNo)
-		        .queryParam("base_date", todayDate)
-		        .queryParam("base_time", baseTime)
-		        .queryParam("nx", "55")
-		        .queryParam("ny", "127")
-		        .queryParam("dataType", dataType);
+				.queryParam("serviceKey", apiKey).queryParam("numOfRows", rows).queryParam("pageNo", pageNo)
+				.queryParam("base_date", todayDate).queryParam("base_time", baseTime).queryParam("nx", "55")
+				.queryParam("ny", "127").queryParam("dataType", dataType);
 
-    	WeatherInfoDto weaterInfoDto = new WeatherInfoDto();
-        ResponseEntity<String> response = httpRequest(builder.build(true).toUri(), HttpMethod.GET, null);
-        JSONObject jsonData = new JSONObject(response.getBody());
+		WeatherInfoDto weaterInfoDto = new WeatherInfoDto();
+		ResponseEntity<String> response = httpRequest(builder.build(true).toUri(), HttpMethod.GET, null);
+		JSONObject jsonData = new JSONObject(response.getBody());
 
-        JSONArray items = jsonData.getJSONObject("response").getJSONObject("body").getJSONObject("items").getJSONArray("item");
-        if(items.length() == 0)
-        	throw new ServiceException("날씨 API에서 정보를 받는데 실패했습니다.");
+		JSONArray items = jsonData.getJSONObject("response").getJSONObject("body").getJSONObject("items")
+				.getJSONArray("item");
+		if (items.length() == 0)
+			throw new ServiceException("날씨 API에서 정보를 받는데 실패했습니다.");
 
-        for(Object item : items) {
-        	JSONObject itemObj = (JSONObject)item;
-        	String category = itemObj.get("category").toString();
-        	if(category.equals("TMP")) {
-        		weaterInfoDto.setTemp(itemObj.get("fcstValue").toString());
-        		weaterInfoDto.setDay(itemObj.get("baseDate").toString());
-        		weaterInfoDto.setTime(itemObj.get("fcstTime").toString());
-        	}else if(category.equals("PTY")) {
-        		String weatherCode = itemObj.get("fcstValue").toString();
-        		String weatherStatus = "";
-        		switch (weatherCode) {
-					case "0": weatherStatus = "이상 없음";
-						break;
-					case "1": weatherStatus = "비";
-						break;
-					case "2": weatherStatus = "눈/비";
-						break;
-					case "3": weatherStatus = "눈";
-						break;
-					case "4": weatherStatus = "소나기";
-						break;
-					default:
-						break;
+		for (Object item : items) {
+			JSONObject itemObj = (JSONObject) item;
+			String category = itemObj.get("category").toString();
+			if (category.equals("TMP")) {
+				weaterInfoDto.setTemp(itemObj.get("fcstValue").toString());
+				weaterInfoDto.setDay(itemObj.get("baseDate").toString());
+				weaterInfoDto.setTime(itemObj.get("fcstTime").toString());
+			} else if (category.equals("PTY")) {
+				String weatherCode = itemObj.get("fcstValue").toString();
+				String weatherStatus = "";
+				switch (weatherCode) {
+				case "0":
+					weatherStatus = "이상 없음";
+					break;
+				case "1":
+					weatherStatus = "비";
+					break;
+				case "2":
+					weatherStatus = "눈/비";
+					break;
+				case "3":
+					weatherStatus = "눈";
+					break;
+				case "4":
+					weatherStatus = "소나기";
+					break;
+				default:
+					break;
 				}
-        		weaterInfoDto.setWeatherStatus(weatherStatus);
-        	}
-        }
+				weaterInfoDto.setWeatherStatus(weatherStatus);
+			}
+		}
 
+		return weaterInfoDto;
+	}
 
-        return weaterInfoDto;
+	// 추후 DB에 minTemp,maxTemp별로 옷차림 넣고 where temp 로 가져오게 변경
+	public String getWearRecommandForWeather(int temp) {
+		String recommandWear = "";
+		// WearRecommandDto wearRecommandDto = new WearRecommandDto();
+		// 27 ~ : 나시티, 반바지, 민소매, 원피스
+		// 23 ~ 26 : 반팔, 얇은셔츠 ,얇은 긴팔, 반바지, 면바지
+		// 20~22 : 긴팔티, 가디건, 후드티, 면바지, 슬랙스, 청바지
+		// 17~19 : 니트, 가디건, 후드티, 맨투맨, 청바지, 면바지, 슬랙스, 원피스
+		// 12~18 : 자켓, 셔츠, 가디건, 간절기 야상
+		// 10~11 : 트렌치코트, 간절기 야상, 여러겹 껴입기
+		// 6~9 : 코트, 가죽자켓
+		// ~5 : 겨울옷 ( 야상, 패딩, 목도리 등등 )
+		if(temp > 27) { // 임시로 if 처리
+			recommandWear = "나시티, 반바지, 민소매, 원피스";
+		}else if( 26 > temp && temp > 23) {
+			recommandWear = "반팔, 얇은셔츠 ,얇은 긴팔, 반바지, 면바지";
+		}else if( 26 > temp && temp > 23) {
+			recommandWear = "반팔, 얇은셔츠 ,얇은 긴팔, 반바지, 면바지";
+		}else if( 22 > temp && temp > 20) {
+			recommandWear = "긴팔티, 가디건, 후드티, 면바지, 슬랙스, 청바지";
+		}else if( 19 > temp && temp > 17) {
+			recommandWear = "니트, 가디건, 후드티, 맨투맨, 청바지, 면바지, 슬랙스, 원피스";
+		}else if( 11 > temp && temp > 10) {
+			recommandWear = "트렌치코트, 간절기 야상, 여러겹 껴입기";
+		}else if( 9 > temp && temp > 6) {
+			recommandWear = "코트, 가죽자켓";
+		}else if( 5 > temp) {
+			recommandWear = "겨울옷 ( 야상, 패딩, 목도리 등등 )";
+		}
+
+		return recommandWear;
 	}
 }
